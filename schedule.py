bff6353df061f3dbdfd286890f7aa0acbe819161
Mike Caron <caron.mike@outlook.com>
2024-04-19 16:40:59 -0400

    do I know how this works?

--- schedule.py
+++ schedule.py
@@ -84,30 +84,33 @@ def main():
     """Shows basic usage of the Sheets API.
     Prints values from a sample spreadsheet.
     """
+    creds = None
 
-    # try relying on existing auth (for github actions)
-    token_path = os.getenv("GOOGLE_GHA_CREDS_PATH") or "token.json"
+    # outside of github actions, we need to auth manually
+    if not os.getenv("GOOGLE_GHA_CREDS_PATH"):
+        # try relying on existing auth (for github actions)
+        token_path =  "token.json"
 
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists(token_path):
-        creds = Credentials.from_authorized_user_file(token_path, SCOPES)
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists(token_path):
+            creds = Credentials.from_authorized_user_file(token_path, SCOPES)
 
-    if not creds and os.getenv("GOOGLE_AUTH"):
-        # do not try and fall back, it won't work, just explode now
-        raise Exception("Unable to use google auth credentials")
+        if not creds and os.getenv("GOOGLE_AUTH"):
+            # do not try and fall back, it won't work, just explode now
+            raise Exception("Unable to use google auth credentials")
 
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open("token.json", "w") as token:
-            token.write(creds.to_json())
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open("token.json", "w") as token:
+                token.write(creds.to_json())
 
     try:
         service = build("sheets", "v4", credentials=creds)
