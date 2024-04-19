817b0fbe8261067919dbb40fbd3305f6e0f7661d
Mike Caron <caron.mike@outlook.com>
2024-04-19 16:56:53 -0400

    start updating keys.py as well (but it needs more work)

--- keys.py
+++ keys.py
@@ -2,9 +2,7 @@ from io import TextIOWrapper
 import os.path
 import os
 
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
+from auth import get_creds
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
@@ -39,9 +37,6 @@ def write_game(f: TextIOWrapper, moby_game, game: ListGame):
     f.write("        </div>\n")
 
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
-
 # The ID and range of a sample spreadsheet.
 SAMPLE_SPREADSHEET_ID = "1YKfxIgEyYlPkm0lvzk5wCMz3Kh6X6PK3GbrWGWHD0sA"
 SAMPLE_RANGE_NAME = "GAMES!A:B"
@@ -54,25 +49,8 @@ def main():
 
     translator.load()
 
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open("token.json", "w") as token:
-            token.write(creds.to_json())
-
     try:
-        service = build("sheets", "v4", credentials=creds)
+        service = build("sheets", "v4", credentials=get_creds())
 
         # Call the Sheets API
         sheet = service.spreadsheets()
--- schedule.py
+++ schedule.py
@@ -5,11 +5,7 @@ import os.path
 import os
 
 from typing import List
-
-from google.auth.transport.requests import Request
-from google.oauth2 import service_account
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
+from auth import get_creds
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
@@ -72,9 +68,6 @@ def write_game(f: TextIOWrapper, game: ListGame):
     f.write("        </div>\n")
 
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
-
 SPREADSHEET_ID = os.getenv("SPREADSHEET_ID")
 SPREADSHEET_NAME = os.getenv("SPREADSHEET_NAME")
 SPREADSHEET_RANGE = SPREADSHEET_NAME + "!" + os.getenv("SPREADSHEET_RANGE")
@@ -84,36 +77,9 @@ def main():
     """Shows basic usage of the Sheets API.
     Prints values from a sample spreadsheet.
     """
-    creds = None
-
-    # outside of github actions, we need to auth manually
-    if not os.getenv("GOOGLE_GHA_CREDS_PATH"):
-        # try relying on existing auth (for github actions)
-        token_path =  "token.json"
-
-        # The file token.json stores the user's access and refresh tokens, and is
-        # created automatically when the authorization flow completes for the first
-        # time.
-        if os.path.exists(token_path):
-            creds = Credentials.from_authorized_user_file(token_path, SCOPES)
-
-        if not creds and os.getenv("GOOGLE_AUTH"):
-            # do not try and fall back, it won't work, just explode now
-            raise Exception("Unable to use google auth credentials")
-
-        # If there are no (valid) credentials available, let the user log in.
-        if not creds or not creds.valid:
-            if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
-            else:
-                flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
-                creds = flow.run_local_server(port=0)
-            # Save the credentials for the next run
-            with open("token.json", "w") as token:
-                token.write(creds.to_json())
 
     try:
-        service = build("sheets", "v4", credentials=creds)
+        service = build("sheets", "v4", credentials=get_creds())
 
         # First fetch the current sheet values
         sheet = service.spreadsheets()
